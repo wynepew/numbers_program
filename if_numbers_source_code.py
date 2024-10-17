d23a59fffddfff64045f707f11f88e9778f66bdd
wynepew <julianwyne12@gmail.com>
2024-10-17 23:19:23 +0800

    finished source code but still need to improve on some factors or creativity

--- if_numbers_source_code.py
+++ if_numbers_source_code.py
@@ -14,16 +14,30 @@
     #if the greater var is determined
     #print var n
 
-def find_highest_number(var1, var2, var3, var4, var5):
-    if var1 > var2:
-        return var1
-    elif var1 > var3:
-        return var1
-    elif var1 > var4:
-        return var1
-    elif var1 > var5:
-        return var1
-    
+# Step 1: Define 5 variables
+var1 = int(input("Please enter a number for var1: "))
+var2 = int(input("Please enter a number for var2: "))
+var3 = int(input("Please enter a number for var3: "))
+var4 = int(input("Please enter a number for var4: "))
+var5 = int(input("Please enter a number for var5: "))
 
-result = find_highest_number(5, 4, 3, 2, 1)
-print(result)
+# Step 2: Compare var1 and var2
+if var1 > var2:
+    highest = var1
+else:
+    highest = var2
+
+# Step 3: Compare the highest with var3
+if highest < var3:
+    highest = var3
+
+# Step 4: Compare the highest with var4
+if highest < var4:
+    highest = var4
+
+# Step 5: Compare the highest with var5
+if highest < var5:
+    highest = var5
+
+# Step 6: Print the highest number
+print(f"The highest number is: {highest}")
